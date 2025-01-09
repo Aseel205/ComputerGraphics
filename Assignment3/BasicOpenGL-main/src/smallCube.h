f380ec3fbafc929c5c28aac31aeeec7e4404e51f
Aseel205 <aseelbiadse1@gmail.com>
2025-01-09 22:57:54 +0200

    push push

---
+++ Assignment3/BasicOpenGL-main/src/smallCube.h
@@ -0,0 +1,38 @@
+#ifndef SMALLCUBE_H
+#define SMALLCUBE_H
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <array>
+
+class SmallCube {
+    
+    public:
+    glm::mat4 modelMatrix;
+    glm::mat4 RotationMatrix  ;              // Model matrix for transformations
+    int index ; 
+
+
+    // Constructor
+    SmallCube(const glm::vec3& pos , int index);
+    SmallCube() ; 
+
+    // Getters
+    glm::vec3 getPosition() const;
+    glm::mat4 getModelMatrix() const;   // Getter for the model matrix
+
+
+
+    // Setters
+    void setPosition(const glm::vec3& pos);
+    void setModelMatrix(const glm::mat4& matrix); // Setter for the model matrix
+    void setRotationMatrix(const glm::mat4& matrix); // Setter for the model matrix
+
+
+
+
+};
+
+
+#endif // SMALLCUBE_H
+
